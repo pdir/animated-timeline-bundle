727be3a0ca74ad558e27813a6388c1f2af2ebe4f
Philipp Seibt <seibt@pdir.de>
2019-11-12 18:03:23 +0100

    update template, css and js

--- src/Resources/public/js/timeline-slider.js
+++ src/Resources/public/js/timeline-slider.js
@@ -1,35 +1,32 @@
 jQuery(document).ready( function($) {
 
-  if( $('.ce_timelineSliderStart').length > 0 ) {
+  if( $('.ce_timelineSliderStart.vertical').length > 0 ) {
 
-    var isMobile = false;
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      isMobile = true;
-    }
+    $(document).scroll( function() {
+      var top_of_element = $(".ce_timelineSliderStart.vertical").offset().top + 30;
+      var bottom_of_element = $(".ce_timelineSliderStart.vertical").offset().top + $(".ce_timelineSliderStart.vertical").outerHeight();
+      var bottom_of_screen = $(window).scrollTop() + $(window).height();
+      var top_of_screen = $(window).scrollTop();
 
-    //if(isMobile === false) {
-      $(document).scroll( function() {
-        var top_of_element = $(".ce_timelineSliderStart").offset().top + 30;
-        var bottom_of_element = $(".ce_timelineSliderStart").offset().top + $(".ce_timelineSliderStart").outerHeight();
-        var bottom_of_screen = $(window).scrollTop() + $(window).height();
-        var top_of_screen = $(window).scrollTop();
+      if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+        $('.ce_timelineSliderStart.vertical > div').addClass('roadmap--initialized');
+        setRoadmapHeight();
+      } else {
+        $('.ce_timelineSliderStart.vertical > div').removeClass('roadmap--initialized');
+      }
+    });
 
-        if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-          $('.ce_timelineSliderStart > div').addClass('roadmap--initialized');
-          setRoadmapHeight();
-        } else {
-          $('.ce_timelineSliderStart > div').removeClass('roadmap--initialized');
-        }
-      });
-    //}
+    $(".ce_timelineSliderStart.vertical").on('click', '.roadmap__navigation a', function() {
+      console.log("click");
+      setRoadmapHeight();
+    });
 
     setRoadmapHeight();
 
     function setRoadmapHeight() {
       setTimeout(function(){
-        $("li.roadmap__events__event").each( function() {
+        $(".ce_timelineSliderStart.vertical li.roadmap__events__event").each( function() {
           var contentHeight = $(this).find(".event__content").height();
-          console.log( contentHeight );
           $(this).css("height",contentHeight+"px");
         });
       }, 200);
