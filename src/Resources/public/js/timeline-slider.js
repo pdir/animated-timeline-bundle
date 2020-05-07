1792591c1a6d3438b912d6317156a4a578917099
Philipp Seibt <seibt@pdir.de>
2020-05-07 15:57:45 +0200

    update script, css and template

--- src/Resources/public/js/timeline-slider.js
+++ src/Resources/public/js/timeline-slider.js
@@ -10,26 +10,32 @@ jQuery(document).ready( function($) {
 
       if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
         $('.ce_timelineSliderStart.vertical > div').addClass('roadmap--initialized');
-        setRoadmapVerticalHeight();
+        setTimeout(function(){
+          setRoadmapVerticalHeight();
+        }, 100);
       } else if( top_of_screen < bottom_of_element ) {
         $('.ce_timelineSliderStart.vertical > div').removeClass('roadmap--initialized');
       }
     });
 
     $(".ce_timelineSliderStart.vertical").on('click', '.roadmap__navigation a', function() {
-      setRoadmapVerticalHeight();
+      setTimeout(function(){
+        setRoadmapVerticalHeight();
+      }, 1000);
     });
 
-    setRoadmapVerticalHeight();
+    setTimeout(function(){
+      setRoadmapVerticalHeight();
+    }, 100);
   }
 
   function setRoadmapVerticalHeight() {
-    setTimeout(function(){
+
       $(".ce_timelineSliderStart.vertical li.roadmap__events__event").each( function() {
         var contentHeight = $(this).find(".event__content").height();
         $(this).css("height",contentHeight+"px");
       });
-    }, 100);
+
   }
 
   if( $('.ce_timelineSliderStart:not(.vertical)').length > 0 ) {
@@ -53,31 +59,35 @@ jQuery(document).ready( function($) {
       var heightOdd = 0;
       var heightEven = 0;
 
-      $(".ce_timelineSliderStart:not(.vertical) li.roadmap__events__event:nth-child(odd)").each( function() {
-        var event = $(this).find(".event");
-        var eventHeight = event.height() + parseFloat(event.css('top').replace('px'));
+      $(".ce_timelineSliderStart:not(.vertical)").each( function() {
 
-        if(eventHeight > heightOdd) {
-          heightOdd = eventHeight;
-        }
+        $(this).find("li.roadmap__events__event:nth-child(odd)").each( function() {
+          var event = $(this).find(".event");
+          var eventHeight = event.height() + parseFloat(event.css('top').replace('px'));
+
+          if(eventHeight > heightOdd) {
+            heightOdd = eventHeight;
+          }
+        });
+        $(this).find("li.roadmap__events__event:nth-child(odd)").height(heightOdd);
+        $(this).find(".roadmap__events").css("padding-bottom",heightOdd + "px");
+
+        $(this).find("li.roadmap__events__event:nth-child(even)").each( function() {
+          var event = $(this).find(".event");
+          var eventHeight = event.height() + parseFloat(event.css('bottom').replace('px'));
+
+          if(eventHeight > heightEven) {
+            heightEven = eventHeight;
+          }
+        });
+        $(this).find("li.roadmap__events__event:nth-child(even)").height(heightEven);
+        $(this).find(".roadmap__events").css("padding-top",heightEven + "px");
+
+        var sliderHeight = $(this).height();
+        $(this).height(heightOdd + heightEven);
+      }, 500);
+
       });
-      $(".ce_timelineSliderStart:not(.vertical) li.roadmap__events__event:nth-child(odd)").height(heightOdd);
-      $(".ce_timelineSliderStart:not(.vertical) .roadmap__events").css("padding-bottom",heightOdd + "px");
-
-      $(".ce_timelineSliderStart:not(.vertical) li.roadmap__events__event:nth-child(even)").each( function() {
-        var event = $(this).find(".event");
-        var eventHeight = event.height() + parseFloat(event.css('bottom').replace('px'));
-
-        if(eventHeight > heightEven) {
-          heightEven = eventHeight;
-        }
-      });
-      $(".ce_timelineSliderStart:not(.vertical) li.roadmap__events__event:nth-child(even)").height(heightEven);
-      $(".ce_timelineSliderStart:not(.vertical) .roadmap__events").css("padding-top",heightEven + "px");
-
-      var sliderHeight = $(".ce_timelineSliderStart:not(.vertical)").height();
-      $(".ce_timelineSliderStart:not(.vertical)").height(heightOdd + heightEven);
-    }, 500);
   }
 
   function setRoadmapAutoHeightMobile() {
