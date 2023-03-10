53c489b7e4ec8cf139eec2405ac44c4409b5bb3a
Mathias Arzberger <arzberger@pdir.de>
2023-03-10 19:36:43 +0100

    run ecs fixer

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -3,17 +3,17 @@
 declare(strict_types=1);
 
 /*
- *  Animated timeline bundle for Contao Open Source CMS
+ * Animated timeline bundle for Contao Open Source CMS
  *
- *  Copyright (c) 2023 pdir / digital agentur // pdir GmbH
+ * Copyright (c) 2023 pdir / digital agentur // pdir GmbH
  *
- *  @package    animated-timeline-bundle
- *  @link       https://pdir.de
- *  @license    LGPL-3.0+
- *  @author     Philipp Seibt <develop@pdir.de>
+ * @package    animated-timeline-bundle
+ * @link       https://pdir.de
+ * @license    LGPL-3.0+
+ * @author     Philipp Seibt <develop@pdir.de>
  *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 $GLOBALS['TL_LANG']['tl_content']['timeline_legend'] = 'Animated Timeline Einstellungen';
