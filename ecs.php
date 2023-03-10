bde0366aee77bfc30b07aa1da38a0b578e4ab0ab
Mathias Arzberger <arzberger@pdir.de>
2023-03-10 19:33:15 +0100

    fix ecs

--- ecs.php
+++ ecs.php
@@ -3,36 +3,37 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 $date = date('Y');
 
 $GLOBALS['ecsHeader'] = <<<EOF
  Animated timeline bundle for Contao Open Source CMS
- 
+
  Copyright (c) $date pdir / digital agentur // pdir GmbH
- 
+
  @package    animated-timeline-bundle
  @link       https://pdir.de
  @license    LGPL-3.0+
  @author     Philipp Seibt <develop@pdir.de>
- 
+
  For the full copyright and license information, please view the LICENSE
  file that was distributed with this source code.
 EOF;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__.'/vendor/contao/easy-coding-standard/config/set/contao.php');
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([__DIR__.'/tools/ecs/vendor/contao/easy-coding-standard/config/contao.php']);
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::LINE_ENDING, "\n");
+    $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
+        'header' => $GLOBALS['ecsHeader'],
+    ]);
 
-    $services = $containerConfigurator->services();
-    $services
-        ->set(HeaderCommentFixer::class)
-        ->call('configure', [[
-            'header' => $GLOBALS['ecsHeader'],
-        ]])
-    ;
+    $ecsConfig->parallel();
+    $ecsConfig->lineEnding("\n");
+
+    $parameters = $ecsConfig->parameters();
+    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_default_cache');
 };
