02e8bfe11055af9f1be09f2de75fceafd8db341a
cgoIT <info@cgo-it.de>
2024-10-21 07:59:21 +0200

    fix: add TimelineElementListeners for multiSRC field handling
    Introduced a new listener class TimelineElementListeners to handle the multiSRC field for timeline elements. Also updated the services configuration and removed redundant entry from tl_content. This ensures the isGallery flag is dynamically set based on the element type.

---
+++ src/EventListener/DataContainer/TimelineElementListeners.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pdir\AnimatedTimelineBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\DataContainer;
+
+class TimelineElementListeners
+{
+    #[AsCallback('tl_content', 'fields.multiSRC.load')]
+    public function setMultiSRCFlags(mixed $varValue, DataContainer $dc): mixed
+    {
+        if ($dc->activeRecord && $dc->activeRecord->type === 'timelineSliderElement') {
+            $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['isGallery'] = true;
+        }
+        return $varValue;
+    }
+}
